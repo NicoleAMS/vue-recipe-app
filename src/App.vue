e572a461a1e5df93bc337eb23343d24833d6355f
NicoleAMS <nicole.sentis@gmail.com>
2021-02-17 17:04:31 +0100

    adds site header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="nav">
+  <the-header></the-header>
+  <!-- <div id="nav">
     <router-link to="/recipes">Recipes</router-link> |
     <router-link to="/recipes/10">Recipe Details</router-link>
-  </div>
+  </div> -->
   <router-view />
 </template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue"; 
+
+export default {
+  components: {
+    TheHeader 
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -23,8 +34,16 @@
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #CF2137;
     }
   }
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,85 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/" id="logo-link"
+        ><img class="logo" :src="siteLogo" alt=""
+      /></router-link>
+      <ul>
+        <li>
+          <router-link to="/recipes">Recipes</router-link>
+        </li>
+        <li>
+          <router-link to="/">Signup</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+import image from "@/assets/recipe.png";
+
+export default {
+  computed: {
+    siteLogo() {
+      return image;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  height: 100px;
+  background-color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  nav {
+    width: 90%;
+    height: 100%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .logo {
+    height: 80px;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    li {
+      margin: 0 0.5rem;
+    }
+
+    a {
+      font-weight: bold;
+      color: white;
+      text-decoration: none;
+      display: inline-block;
+      padding: 0.75rem 1.5rem;
+      border: 1px solid transparent;
+    }
+
+    a#logo-id {
+      height: 80px;
+    }
+
+    a:active,
+    a:hover,
+    a.router-link-active {
+      border: 1px solid #cf2137;
+    }
+  }
+}
+</style>
