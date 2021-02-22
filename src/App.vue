7b35b7b7cc320d6d4cee94effd1bd8fef574b64e
NicoleAMS <nicole.sentis@gmail.com>
2021-02-22 12:04:22 +0100

    adds auto login functionality

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,9 @@ export default {
   components: {
     TheHeader,
   },
+  created() {
+    this.$store.dispatch('autoLogin');
+  }
 };
 </script>
 
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -23,6 +23,14 @@ export default {
       throw error;
     }
 
+    const expiresIn = responseData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
+
+    localStorage.setItem('email', responseData.email);
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
     context.commit('setUser', {
       email: responseData.email,
       userId: responseData.localId,
@@ -30,6 +38,31 @@ export default {
     });
   },
   logout(context) {
+    localStorage.removeItem('email');
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
     context.commit('setUser', null);
+  },
+  autoLogin(context) {
+    const email =  localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
+
+    if (token && userId) {
+      context.commit('setUser', {
+        email: email,
+        userId: userId,
+        token: token
+      });
+    }
   }
 };
